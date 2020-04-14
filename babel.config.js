01442754380be547db0fe239b83144f2f806ac9b
Smit Patel <smitpatel24@gmail.com>
2020-04-14 13:47:31 +0530

    Fix babel compile

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,16 @@ module.exports = {
     [
       '@babel/preset-env',
       {
+        useBuiltIns: "entry",
         targets: {
           node: 'current',
         },
+        corejs: 2
       },
     ],
   ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-export-default-from"
+  ]
 };
--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,10 @@
 
 var _identity = _interopRequireDefault(require("./identity"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = {
-  identity: function identity(credentials) {
-    return new _identity["default"](credentials);
+  identity: function (credentials) {
+    return new _identity.default(credentials);
   }
 };
