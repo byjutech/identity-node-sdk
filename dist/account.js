a93c42905eeb3b50e18a04f37f7d1919c06fe497
Muthukumar-16 <muthuabja2020@gmail.com>
2021-10-21 15:12:09 +0530

    fix: :bug: Adding account move code in dist

--- dist/account.js
+++ dist/account.js
@@ -12,64 +12,70 @@ var _oauth_token = require("./oauth_token");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class Account {
-  constructor(options) {
-    if (options instanceof _oauth_token.OAuthToken) {
-      this.tokenProvider = options;
-    } else {
-      this.tokenProvider = new _oauth_token.OAuthToken(options);
+    constructor(options) {
+        if (options instanceof _oauth_token.OAuthToken) {
+            this.tokenProvider = options;
+        } else {
+            this.tokenProvider = new _oauth_token.OAuthToken(options);
+        }
+
+        this.serviceBaseUrl = this.tokenProvider.baseUrl;
     }
 
-    this.serviceBaseUrl = this.tokenProvider.baseUrl;
-  }
+    get token() {
+        return this.tokenProvider.token();
+    }
 
-  get token() {
-    return this.tokenProvider.token();
-  }
+    async get(id) {
+        const reqUrl = new URL(`${this.serviceBaseUrl}/api/accounts/${id}?identity_info=true`);
+        const token = await this.token;
+        const { payload } = await _wreck.default.get(reqUrl.href, {
+            json: true,
+            headers: {
+                Authorization: `Bearer ${token.access_token}`,
+            },
+        });
+        return payload;
+    }
 
-  async get(id) {
-    const reqUrl = new URL(`${this.serviceBaseUrl}/api/accounts/${id}?identity_info=true`);
-    const token = await this.token;
-    const {
-      payload
-    } = await _wreck.default.get(reqUrl.href, {
-      json: true,
-      headers: {
-        'Authorization': `Bearer ${token.access_token}`
-      }
-    });
-    return payload;
-  }
+    async add(identityId, body) {
+        const reqUrl = new URL(`${this.serviceBaseUrl}/api/identities/${identityId}/accounts`);
+        const token = await this.token;
+        const { payload } = await _wreck.default.post(reqUrl.href, {
+            json: true,
+            headers: {
+                Authorization: `Bearer ${token.access_token}`,
+            },
+            payload: body,
+        });
+        return payload;
+    }
 
-  async add(identityId, body) {
-    const reqUrl = new URL(`${this.serviceBaseUrl}/api/identities/${identityId}/accounts`);
-    const token = await this.token;
-    const {
-      payload
-    } = await _wreck.default.post(reqUrl.href, {
-      json: true,
-      headers: {
-        'Authorization': `Bearer ${token.access_token}`
-      },
-      payload: body
-    });
-    return payload;
-  }
+    async patch(id, body) {
+        const reqUrl = new URL(`${this.serviceBaseUrl}/api/accounts/${id}`);
+        const token = await this.token;
+        const { payload } = await _wreck.default.patch(reqUrl.href, {
+            json: true,
+            headers: {
+                Authorization: `Bearer ${token.access_token}`,
+            },
+            payload: body,
+        });
+        return payload;
+    }
 
-  async patch(id, body) {
-    const reqUrl = new URL(`${this.serviceBaseUrl}/api/accounts/${id}`);
-    const token = await this.token;
-    const {
-      payload
-    } = await _wreck.default.patch(reqUrl.href, {
-      json: true,
-      headers: {
-        'Authorization': `Bearer ${token.access_token}`
-      },
-      payload: body
-    });
-    return payload;
-  }
-
+    async move(id, body) {
+        const reqUrl = new URL(`${this.serviceBaseUrl}/api/accounts/${id}`);
+        const token = await this.token;
+        const { payload } = await _wreck.default.put(reqUrl.href, {
+            json: true,
+            headers: {
+                Authorization: `Bearer ${token.access_token}`,
+            },
+            payload: body,
+        });
+        return payload;
+    }
 }
 
 var _default = Account;
